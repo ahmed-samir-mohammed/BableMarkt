3262f4778d348409f941c9111b035b084ccb9a2f
Ahmed Samir <samierahmed37@gmil.com>
2020-04-20 01:43:02 +0200

    Add Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ForgetPasswordComponent } from './login/forget-password/forget-password.component';
+import { ForgetPasswordConfirmComponent } from './login/forget-password-confirm/forget-password-confirm.component';
+import { NewPasswordComponent } from './login/new-password/new-password.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'forget-password', component: ForgetPasswordComponent},
+  { path: 'confirm', component: ForgetPasswordConfirmComponent},
+  { path: 'new-password', component: NewPasswordComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    ForgetPasswordComponent,
+    ForgetPasswordConfirmComponent,
+    NewPasswordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
+
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
---
+++ src/app/login/forget-password-confirm/forget-password-confirm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forget-password-confirm',
+  templateUrl: './forget-password-confirm.component.html',
+  styleUrls: ['./forget-password-confirm.component.scss']
+})
+export class ForgetPasswordConfirmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
